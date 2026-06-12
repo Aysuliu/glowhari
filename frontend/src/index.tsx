8a90b7de4d8c0043db1b8be1ef387fdc0d996286
Aysulu <bakhievaays@gmail.com>
2026-06-13 01:47:54 +0900

    feat: develop socket io client

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './app/services/axiosInterceptor';
 import App from './App';
+import { SocketProvider } from './app/context/SocketContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <SocketProvider>
     <App />
+    </SocketProvider>
   </React.StrictMode>
 );
